c4c6499f2d1e4c905ad17180791d4b6b61979ce3
djmousefarm <djmousefarm@googlemail.com>
2016-10-05 12:57:51 +0000

    no longer trying to perform an operation on a file thats not been called yet!

--- program.js
+++ program.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
-var num = undefined;
+var fileText = undefined;
 function getFile(callback) {
 	 fs.readFile(process.argv[2],'utf8', function callback(err,myFile) {
 		if (err) {
 			//console.log(process.argv[2] + " is not ready yet!);
 			} else {
-			console.log(myFile);
-			num = myFile.split('\n').length-1;
+			//console.log(myFile);
+			fileText = myFile1;
 			callback();
 			}	
 		})
 	}
-function output() {console.log(num)};
-getFile(num);
+function output() {console.log(fileText.split('\n').length-1};
+getFile(fileText);
