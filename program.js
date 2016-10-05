4eeaa044c28ab9d5399aec3dba13a6a53d549fcb
djmousefarm <djmousefarm@googlemail.com>
2016-10-05 13:22:30 +0000

    tried removing the callback() statement

--- program.js
+++ program.js
@@ -3,11 +3,11 @@ var fileText = undefined;
 function getFile(callback) {
 	 fs.readFile(process.argv[2],'utf8', function callback(err,myFile) {
 		if (err) {
-			//console.log(process.argv[2] + " is not ready yet!);
+			console.log(process.argv[2] + " is not ready yet!);
 			} else {
 			//console.log(myFile);
 			fileText = myFile;
-			callback();
+			//callback();
 			};	
 		});
 	};
