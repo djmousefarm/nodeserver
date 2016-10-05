32b01347c14bec83c4d3b9d7276e60ded33c692c
djmousefarm <djmousefarm@googlemail.com>
2016-10-05 13:31:39 +0000

    put back in error catching - should work now - and reordered some logic

--- program.js
+++ program.js
@@ -2,12 +2,11 @@ var fs = require('fs');
 var fileText = undefined;
 function getFile(callback) {
 	 fs.readFile(process.argv[2],'utf8', function callback(err,myFile) {
-		if (err) {
-			//console.log(process.argv[2] + " is not ready yet!);
-			//this does nothing at the moment - a placeholder for error catching
-			} else {
-			//console.log(myFile);
+		if (!err) {
 			fileText = myFile;
+			} else {
+			console.error(process.argv[2] + " is not ready yet!");
+			//this does nothing at the moment - a placeholder for error catching			
 			//callback();
 			};	
 		});
