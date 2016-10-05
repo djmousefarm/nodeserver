32f0386beaebd224072244728958094f0512040c
djmousefarm <djmousefarm@googlemail.com>
2016-10-05 11:14:03 +0000

    file system

--- program.js
+++ program.js
@@ -1,6 +1,4 @@
-var arr = process.argv;
-var num = 0;
-for (var i = 2;i < arr.length; i++) {
-	num+=parseInt(arr[i]);
-}
-console.log(num);
+var fs = require('fs');
+var file = fs.readFileSync(process.argv[2]);
+var arr = file.split('\n');
+console.log(arr.length-1);
