4d252cea710f4218c2b14e4eec8a4743b32ff939
djmousefarm <djmousefarm@googlemail.com>
2016-10-05 13:51:38 +0000

    put the callback() back in

--- program.js
+++ program.js
@@ -4,6 +4,7 @@ function getFile(callback) {
 	 fs.readFile(process.argv[2],'utf8', function callback(err,myFile) {
 		if (!err) {
 			fileText = myFile;
+			callback();
 			} else {
 			console.error(process.argv[2] + " is not ready yet!");
 			//this does nothing at the moment - a placeholder for error catching			
