397218a7bb1665621f01eb96b6e7c5a464e98702
djmousefarm <djmousefarm@googlemail.com>
2016-10-05 15:58:21 +0000

    attempt at dir list with extension filter

---
+++ lsFilter.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var dir = process.argv[2];
+var ext = process.argv[3];
+var filteredList = undefined;
+function showList(callback) {
+	fs.readdir(dir, function listReady(err, list) {
+		if (err) {
+			console.error(err);
+		}
+		filteredList = list;
+		callback();
+	});
+};
+function hasExt(value,ext) {if(value.endsWith(ext)) {return value}};
+function lister() {filteredList.filter(hasExt(filteredList,ext)).forEach(function(element){console.log(element)})};
+showList(lister);
