17dfc8d75f10fb89c68fc55eed4f64892ad003f7
djmousefarm <djmousefarm@googlemail.com>
2016-10-06 11:08:01 +0000

    got setEncoding working!

--- httpGetter.js
+++ httpGetter.js
@@ -3,6 +3,7 @@ var http = require('http');
 var url = process.argv[2];
 
 http.get(url, function callback(response) {
+	response.setEncoding('utf8')
 	if (response.error) 
 		console.error('Error : ' + response.error)
 	response.on('data',function(data) {
